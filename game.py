8da2121657f25c2fd55048dddf83baf900f103f8
AlexRobux <65112064+AlexRobux@users.noreply.github.com>
2021-06-11 13:58:23 +0300

    Update game.py

--- game.py
+++ game.py
@@ -4,13 +4,13 @@ playerInput = 0
 myInput = 0
 while sum <= 100:
     sum += myInput
-    print(f"┌──────┬──────────────┬────────┐\n│player│  sum         │ pc     │\n├──────┼──────────────┼────────┤\n│      │     {sum}", " " * (7 - len(str(sum))), f"│   +{myInput}", " " * (2 - len(str(myInput))), f"│\n└──────┴──────────────┴────────┘")
+    print(f"┌──────┬──────────────┬────────┐\n│Player│  Total       │ PC     │\n├──────┼──────────────┼────────┤\n│      │     {sum}", " " * (7 - len(str(sum))), f"│   +{myInput}", " " * (2 - len(str(myInput))), f"│\n└──────┴──────────────┴────────┘")
     if (sum >= 100):
         print("PC WIN")
         break
     playerInput = int(input("Player input:"))
     sum += playerInput
-    print(f"┌──────┬──────────────┬────────┐\n│player│  sum         │ pc     │\n├──────┼──────────────┼────────┤\n│ +{playerInput}", " " * (2 - len(str(playerInput))),f"│     {sum}", " " * (7 - len(str(sum))), f"│        │\n└──────┴──────────────┴────────┘")
+    print(f"┌──────┬──────────────┬────────┐\n│Player│  Total       │ PC     │\n├──────┼──────────────┼────────┤\n│ +{playerInput}", " " * (2 - len(str(playerInput))),f"│     {sum}", " " * (7 - len(str(sum))), f"│        │\n└──────┴──────────────┴────────┘")
     if playerInput > 10 or playerInput < 1:
         print ("Don't break the rules")
         break
